060d66c89028d951dbdaf1ffb0f87dcee0fd23ad
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-20 17:24:40 -0800

    First C# function correctly executing

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -6,11 +6,15 @@ public class Hacker : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        ShowMainMenu();
+	}
+
+    void ShowMainMenu() {
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Enter your selection: ");
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
