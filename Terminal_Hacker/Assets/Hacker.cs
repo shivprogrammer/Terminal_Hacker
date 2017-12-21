d4e7e1971771c1750f2abed362bd2dbb4ce8adb7
shivprogrammer <shiv.unadkat@gmail.com>
2017-12-20 17:56:35 -0800

    working with some variables

--- Terminal_Hacker/Assets/Hacker.cs
+++ Terminal_Hacker/Assets/Hacker.cs
@@ -2,20 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Hacker : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-        ShowMainMenu();
-	}
+    // Use this for initialization
+    void Start () {
+        ShowMainMenu("Hello Shivvy");
+    }
 
-    void ShowMainMenu() {
+    void ShowMainMenu(string greeting) {
+        Terminal.ClearScreen();
+        Terminal.WriteLine(greeting);
         Terminal.WriteLine("What would you like to hack into?");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Enter your selection: ");
     }
-	
+
 	// Update is called once per frame
 	void Update () {
 		
